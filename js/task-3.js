5821c1dc0d894776651a82254e5a01c7c3e9a3fd
oshkurupii <oshkurupij@gmail.com>
2024-03-07 21:30:04 +0100

    goit-js-hw-01

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+  const result =
+    Number.parseFloat(content) +
+    2 * Number.parseFloat(padding) +
+    2 * Number.parseFloat(border);
+  return result;
+}
+
+// results
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
